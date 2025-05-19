c5ca02b34dc6476fa07cb8c3133338744dff2b79
Ryan Clark <98057867+clarkvd@users.noreply.github.com>
2025-05-19 13:32:59 -0400

    Add files via upload

--- signalframe/__init__.py
+++ signalframe/__init__.py
@@ -5,7 +5,7 @@ from .stats import compare_signal_groups, run_one_way_anova, run_two_way_anova
 from .plot import plot_signal_region, plot_signals_from_bed
 
 __all__ = [
-    "load_bed", "expand_bed_regions",
+    "load_bed", "expand_bed_regions", "save_bed",
     "compute_signal", "compute_signal_multi",
     "normalize_signal", "compare_tracks", "sort_signal_df",
     "compare_signal_groups", "run_one_way_anova", "run_two_way_anova",
